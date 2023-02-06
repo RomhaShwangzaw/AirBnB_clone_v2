ebc97bb7be6d4d4f2cedc1823dd779a030399428
Romha Shwangzaw <romhas@gmail.com>
2023-02-06 15:35:25 +0300

    Bug Fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
-"""Starts a flask app
-    listens to 0.0.0.0:5000
-"""
+"""List all states Module"""
+
 from models import storage
 from models.state import State
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route('/states_list')
 def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
-    States are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
+    """display a HTML the States"""
+    all_states = list(storage.all(State).values())
+    return (render_template('7-states_list.html', all_states=all_states))
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown(self):
+    """function that call close methofd"""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
