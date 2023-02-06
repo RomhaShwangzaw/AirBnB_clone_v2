b99df73d76883d355b8f9a2b53daf37649996dd3
Romha Shwangzaw <romhas@gmail.com>
2023-02-06 15:26:35 +0300

    Bug Fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,8 +3,8 @@
     listens to 0.0.0.0:5000
 """
 from models import storage
-from flask import Flask
-from flask import render_template
+from models.state import State
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
