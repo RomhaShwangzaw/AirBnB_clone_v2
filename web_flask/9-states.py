082858d0380853f37686419efbc26d969d9994f2
Romha Shwangzaw <romhas@gmail.com>
2023-02-06 16:07:01 +0300

    Bug Fix

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -6,8 +6,8 @@ Routes:
     /states/<id>: HTML page displaying the given state with <id>.
 """
 from models import storage
-from flask import Flask
-from flask import render_template
+from models.state import State
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -17,21 +17,21 @@ def states():
     """Displays an HTML page with a list of all States.
     States are sorted by name.
     """
-    states = storage.all("State")
+    states = storage.all(State)
     return render_template("9-states.html", state=states)
 
 
 @app.route("/states/<id>", strict_slashes=False)
 def states_id(id):
     """Displays an HTML page with info about <id>, if it exists."""
-    for state in storage.all("State").values():
+    for state in storage.all(State).values():
         if state.id == id:
             return render_template("9-states.html", state=state)
     return render_template("9-states.html")
 
 
 @app.teardown_appcontext
-def teardown(exc):
+def teardown(self):
     """Remove the current SQLAlchemy session."""
     storage.close()
 
